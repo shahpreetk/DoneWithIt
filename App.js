99c857360eae645a92ce564410e8ed4c7f30b3dd
shahpreetk <preet25@somaiya.edu>
2020-07-11 13:38:28 +0530

    static card being made

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 // import {} from '@expo/vector-icons';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ListingDetailsScreen />;
 }
