54d2a008b7f6f7a860526ecdab8c4373e558571e
shahpreetk <preet25@somaiya.edu>
2020-07-13 12:16:47 +0530

    refreshing messages screen added

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -23,6 +23,7 @@ const initialMessages = [
 
 export default function MessagesScreen(props) {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id));
@@ -45,6 +46,17 @@ export default function MessagesScreen(props) {
           />
         )}
         ItemSeparatorComponent={ListItemSeperator}
+        refreshing={refreshing}
+        onRefresh={()=>{
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/preet.jpg"),
+            },
+          ])
+        }}
       />
     </Screen>
   );
