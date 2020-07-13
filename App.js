09cd72068510efa8823d3ec8d3a24fea71152871
shahpreetk <preet25@somaiya.edu>
2020-07-13 13:01:26 +0530

    icon component made

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from './app/components/Icon'; 
 // import {} from '@expo/vector-icons';
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+    <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 }
