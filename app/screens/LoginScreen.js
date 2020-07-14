ac530027fc112b31664a0522850543abe7883f1f
shahpreetk <preet25@somaiya.edu>
2020-07-14 11:14:46 +0530

    form field component made

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { Formik } from "formik";
 import * as Yup from 'yup';
 import Screen from "../components/Screen";
-import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
-import AppText from "../components/AppText";
-import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -24,28 +22,24 @@ export default function LoginScreen() {
       >
         {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
               keyboardType="email-address"
-              onBlur={()=>setFieldTouched('email')}
-              onChangeText={handleChange('email')}
+              name='email'
               placeholder="Email"
               textContentType="emailAddress"
             />
-            <ErrorMessage error={errors.email} visible={touched.email} />
-            <AppTextInput
+            <AppFormField
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
-              onBlur={()=>setFieldTouched('password')}
-              onChangeText={handleChange('password')}
               placeholder="Password"
+              name='password'
               secureTextEntry
               textContentType="password"
             />
-            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton
               title="Login"
               onPress={handleSubmit}
