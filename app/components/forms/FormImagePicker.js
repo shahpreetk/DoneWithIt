471ad805e6b68d64ccd8b40e3887585dd47844cc
shahpreetk <preet25@somaiya.edu>
2020-07-18 15:07:49 +0530

    formimagepicker made

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormikContext } from "formik";
+import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
+
+export default function FormImagePicker({name}) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = valuse[name];
+
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  const handleRemove = (uri) => {
+    setFieldValue(name, imageUris.filter((imageUri) => imageUri !== uri));
+  };
+
+  return (
+    <>
+      <ImageInputList imageUris={imageUris} onAddImage={handleAdd} onRemoveImage={handleRemove} />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
