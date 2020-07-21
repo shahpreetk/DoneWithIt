eecb101cc548f4742936096d01cf9219938f03b8
shahpreetk <preet25@somaiya.edu>
2020-07-21 17:14:34 +0530

    feed navigator added so navigation now possible to listings details screen

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, TouchableWithoutFeedback } from "react-native";
 
 import AppText from "./AppText";
 import colors from "../config/colors";
 
-function Card({ title, subTitle, image }) {
+function Card({ title, subTitle, image, onPress }) {
   return (
+    <TouchableWithoutFeedback onPress={onPress } >
     <View style={styles.card}>
       <Image style={styles.image} source={image} />
       <View style={styles.detailsContainer}>
@@ -13,6 +14,7 @@ function Card({ title, subTitle, image }) {
         <AppText style={styles.subTitle}>{subTitle}</AppText>
       </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,36 +1,46 @@
-import React from 'react'
-import { StyleSheet, FlatList } from 'react-native'
-import Screen from '../components/Screen'
-import Card from '../components/Card'
-import colors from '../config/colors';
+import React from "react";
+import { StyleSheet, FlatList } from "react-native";
+import Screen from "../components/Screen";
+import Card from "../components/Card";
+import colors from "../config/colors";
 
 const listings = [
-    {
-        id: 1,
-        title: 'Red Jacket for sale',
-        price: 100,
-        image: require('../assets/jacket.jpg')
-    },
-    {
-        id: 2,
-        title: 'Couch in great condition',
-        price: 1000,
-        image: require('../assets/couch.jpg')
-    }
+  {
+    id: 1,
+    title: "Red Jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: 1000,
+    image: require("../assets/couch.jpg"),
+  },
 ];
 
-export default function ListingsScreen() {
-    return (
-        <Screen style={styles.screen}>
-            <FlatList data={listings} keyExtractor={listing => listing.id.toString()}
-            renderItem={({ item })  => <Card title={item.title} subTitle={"$" +  item.price} image={item.image} />} />
-        </Screen>
-    )
+export default function ListingsScreen({ navigation }) {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$" + item.price}
+            image={item.image}
+            onPress={()=>navigation.navigate("ListingDetails", item)}
+          />
+        )}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen:{
-        padding: 20,
-        backgroundColor: colors.light,
-    }
-})
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
