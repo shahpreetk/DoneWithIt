44d3c547dc5e0d708c535072ec12389168e456d0
shahpreetk <preet25@somaiya.edu>
2020-07-27 12:17:33 +0530

    api added

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,25 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList } from "react-native";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import colors from "../config/colors";
-
-const listings = [
-  {
-    id: 1,
-    title: "Red Jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+import listingsApi from "../api/listings";
 
 export default function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -29,8 +26,8 @@ export default function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
-            onPress={()=>navigation.navigate("ListingDetails", item)}
+            imageUrl={item.images[0].url}
+            onPress={() => navigation.navigate("ListingDetails", item)}
           />
         )}
       />
