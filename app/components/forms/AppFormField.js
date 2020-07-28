d3f2d2d6f79643ce8f5ea9b3fa45cac9152c6bcb
shahpreetk <preet25@somaiya.edu>
2020-07-28 19:38:42 +0530

    on posting form clearing done

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -5,13 +5,14 @@ import AppTextInput from '../AppTextInput';
 import ErrorMessage from './ErrorMessage'; 
 
 export default function ({name, width, ...otherProps }) {
-    const {setFieldTouched, handleChange, errors, touched } = useFormikContext();
+    const {setFieldTouched, setFieldValue, errors, touched, values } = useFormikContext();
 
   return (
     <>
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={text=>setFieldValue(name,text)}
+        value={values[name]}
         width={width}
         {...otherProps}
       />
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -84,7 +84,7 @@ function ListingEditScreen() {
   const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, {resetForm}) => {
     setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -96,6 +96,8 @@ function ListingEditScreen() {
       setUploadVisible(false);
       return alert("Could not save the listing:(");
     }
+
+    resetForm();
   };
 
   return (
