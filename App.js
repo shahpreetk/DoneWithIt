e0af1d56c70d01a7cfe857d5f8dbcef25b008a93
shahpreetk <preet25@somaiya.edu>
2020-07-29 23:35:15 +0530

    logging in possible now

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { AppLoading } from "expo";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import AuthContext from "./app/auth/context";
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
